7136214e96dc7c091387fb8d3ec1cf9a53f942ad
Chathura Colombage <chathura@Chathuras-MacBook-Pro.local>
2014-06-21 23:14:41 +0100

    Started work on Sudoku puzzle solving

--- src/Main.hs
+++ src/Main.hs
@@ -1,1 +1,13 @@
+cross xs ys = [[x,y] | x <- xs, y <- ys]
+
+digits = "123456789"
+rows = "ABCDEFGHI"
+cols = digits
+
+squares = cross rows cols
+
+unitlist = [cross rows [c] | c <- cols] ++
+           [cross rows [c] | c <- rows] ++
+           [cross rs cs | rs <- ["ABC", "DEF", "GHI"], cs <- ["123", "456", "789"]]
+
 main = putStrLn "Simple Sudoku Solver"
