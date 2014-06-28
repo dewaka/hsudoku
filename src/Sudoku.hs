e14da2b835c7c8e3e060d4a86c048730acfd2e33
Chathura Colombage <dcdewaka@gmail.com>
2014-06-29 00:27:47 +0100

    Sudoku project changes

---
+++ src/Sudoku.hs
@@ -0,0 +1,22 @@
+module Sudoku where
+
+cross xs ys = [[x,y] | x <- xs, y <- ys]
+
+digits = "123456789"
+rows = "ABCDEFGHI"
+
+cols = digits
+
+squares = cross rows cols
+
+unitlist = [cross rows [c] | c <- cols] ++
+           [cross [r] cols | r <- rows] ++
+           [cross rs cs | rs <- ["ABC", "DEF", "GI"]
+                        , cs <- ["123", "456", "789"]]
+
+unitsOf x = [u | u <- unitlist, x `elem` u]
+
+peersOf s = foldl addNew [] $ concat $ unitsOf s
+  where
+    addNew xs x = if x == s || x `elem` xs then xs else x:xs
+              
