e7c1dabbfcdd20dcff0a48519d0dbb65bdf5faf6
Chathura Colombage <dcdewaka@gmail.com>
2014-06-28 23:28:38 +0100

    First definition

--- src/Main.hs
+++ src/Main.hs
@@ -2,17 +2,21 @@ cross xs ys = [[x,y] | x <- xs, y <- ys]
 
 digits = "123456789"
 rows = "ABCDEFGHI"
+
 cols = digits
 
 squares = cross rows cols
 
 unitlist = [cross rows [c] | c <- cols] ++
-           [cross rows [c] | c <- rows] ++
-           [cross rs cs | rs <- ["ABC", "DEF", "GHI"],
-                          cs <- ["123", "456", "789"]]
+           [cross [r] cols | r <- rows] ++
+           [cross rs cs | rs <- ["ABC", "DEF", "GI"]
+                        , cs <- ["123", "456", "789"]]
 
-units = undefined
+unitsOf x = [u | u <- unitlist, x `elem` u]
 
-peers = undefined
-
-main = putStrLn "Simple Sudoku Solver"
+peersOf s = foldl addNew [] $ concat $ unitsOf s
+  where
+    addNew xs x = if x == s || x `elem` xs then xs else x:xs
+              
+main = do
+  putStrLn "Simple Sudoku Solver"
