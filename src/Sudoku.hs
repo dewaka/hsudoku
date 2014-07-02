3a8dd5cbf171e8a9e0f516ce4ea258f5f4a1cffa
Chathura Colombage <dcdewaka@gmail.com>
2014-07-02 23:08:38 +0100

    gridValues definition

--- src/Sudoku.hs
+++ src/Sudoku.hs
@@ -20,6 +20,17 @@ peersOf s = foldl addNew [] $ concat $ unitsOf s
   where
     addNew xs x = if x == s || x `elem` xs then xs else x:xs
 
-parseGrid = undefined
 
-gridValues = undefined
+assign = undefined
+
+gridValues grid = let vals = [c | c <- grid, c `elem` digits || c `elem` "0."]
+                  in if length vals == 81 then zip squares vals
+                     else error $ "Invalid grid with " ++ (show $ length vals) ++ " values"
+
+parseGrid grid =
+  let vals = zip squares $ repeat digits
+      gvals = gridValues grid
+      check = [assign vals s d | (s, d) <- gvals, d `elem` digits]
+  in if all id check
+     then Just vals
+     else Nothing
